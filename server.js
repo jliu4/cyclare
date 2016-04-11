4f1f073460c2d777e83d279ba32911a9d783c4c5
jliu4 <jliuapril@gmail.com>
2016-04-10 20:51:36 -0700

    '7'

--- server.js
+++ server.js
@@ -1,47 +1,45 @@
+
+var isUseHTTPs = !(!!process.env.PORT || !!process.env.IP);
+
+var server = require(isUseHTTPs ? 'https' : 'http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+
 var static = require('node-static');
-var http = require('http');
 var file = new(static.Server)();
-var app = http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(process.env.PORT || 2013);
+var app;
 
-var io = require('socket.io').listen(app);
-io.sockets.on('connection', function (socket){
+if (isUseHTTPs) {
+    var options = {
+        key: fs.readFileSync(path.join(__dirname, 'keys/server.key')),
+        cert: fs.readFileSync(path.join(__dirname, 'keys/server.crt'))
+    };
 
-	function log(){
-		var array = [">>> Message from server: "];
-	  for (var i = 0; i < arguments.length; i++) {
-	  	array.push(arguments[i]);
-	  }
-	    socket.emit('log', array);
-	}
+    app = server.createServer(options,function (req, res) {
+         file.serve(req, res);
 
-	socket.on('message', function (message) {
-		log('Got message: ', message);
-     //For a real app, should be room only (not broadcast)
-		socket.broadcast.emit('message', message);
-	});
-
-	socket.on('create or join', function (room) {
-		var numClients = io.sockets.adapter.rooms[room]!=undefined ? Object.keys(io.sockets.adapter.rooms[room]).length:0;
-
-		log('Room ' + room + ' has ' + numClients + ' client(s)');
-		log('Request to create or join room', room);
-
-		if (numClients == 0){
-			socket.join(room);
-			socket.emit('created', room);
-		} else if (numClients == 1) {
-			io.sockets.in(room).emit('join', room);
-			socket.join(room);
-			socket.emit('joined', room);
-		} else { // max two clients
-			socket.emit('full', room);
-		}
-		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
-
-	});
-
+})} else app = server.createServer(function (req, res) {
+        file.serve(req, res);
 });
 
+app = app.listen(process.env.PORT || 9001, process.env.IP || "0.0.0.0", function() {
+    var addr = app.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
+});
+
+require('./Signaling-Server.js')(app, function(socket) {
+    try {
+        var params = socket.handshake.query;
+
+        if (!params.socketCustomEvent) {
+            params.socketCustomEvent = 'custom-message';
+        }
+
+        socket.on(params.socketCustomEvent, function(message) {
+            try {
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            } catch (e) {}
+        });
+    } catch (e) {}
+});
